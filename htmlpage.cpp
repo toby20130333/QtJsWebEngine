b67c4ddeb8bceda89579224ed2679f8d34eb47f7
toby520 <tb1210582007@126.com>
2016-09-10 15:38:47 +0800

    first commit

--- htmlpage.cpp
+++ htmlpage.cpp
@@ -7,7 +7,9 @@ HtmlPage::HtmlPage(QWidget *parent) :
     sX = 0 ; sY = 0 ;
     // Set up the communications channel
     this->page()->setWebChannel(&channel) ;
+    //将此单例注入到js端
     channel.registerObject("widget", WebObjectInstance::instance());
+    //连接信号槽
     connect(WebObjectInstance::instance(),SIGNAL(signalCursorMoved(int,int)),this,SLOT(slotJsUpdated(int,int)));
     // Set the page content
     setUrl(QUrl("qrc:/index.html")) ;
@@ -16,21 +18,17 @@ HtmlPage::HtmlPage(QWidget *parent) :
 HtmlPage::~HtmlPage()
 {
 }
-
+///
+/// \brief HtmlPage::slotJsUpdated
+/// \param x
+/// \param y
+///  通过单例更新C++界面内容 同时可以通知js刷新UI
+///
 void HtmlPage::slotJsUpdated(int x, int y)
 {
     qDebug("slotJsUpdated");
      addDot(x,y,6,"#FFFFFF");
 }
-
-// Called when javascript coordinates are updated
-//void HtmlPage::jsUpdated(int x, int y)
-//{
-//    sX = x ; sY = y ;
-////    emit cursorMoved(x, y) ;
-//    emit gWebObject->signalCursorMoved(x,y);
-//    addDot(x, y, 4, "#FFFFFF") ;
-//}
 
 // Call to add a dot at the given x/y coordinate
 void HtmlPage::addDot(int x, int y, int sz, QString colour)
--- htmlpage.h
+++ htmlpage.h
@@ -23,26 +23,11 @@ public:
     virtual ~HtmlPage() ;
 
 public slots:
-//    ///
-//    /// 此处声明多个槽函数用于处理与js之间的交互 供js代码直接调用
-//    /// 也可能接收外界C++来的数据 通过信号槽链接
-//    ///
     void slotJsUpdated(int x, int y) ;// Callback from javascript (save X & Y)
-
-//    void cplusplusUpdate(const QVariant& var);// connect slot from c++
-//signals:
-//    /// 此处声明多个信号与外界C++进行通知
-//    void cursorMoved(int x, int y) ; // Indicate cursor has moved
-
 private:
     int sX, sY ;                     // X and Y coordinate stores
     QWebChannel channel ;  // Channel for C++ to Javascript comms
-
 private:
-    ///
-    /// 此处声明多个私有方法
-    /// 通过c++注入代码字符串进行通知js
-    ///
     void addDot(int x, int y, int sz, QString colour) ;   // Creates dot at current X and Y
 };
 
